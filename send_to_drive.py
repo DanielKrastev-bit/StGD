f871201ba5e325efc39f80ecc4ee5f3496e1d76f
danielkrastev-bit <danielkrastev2007@gmail.com>
2024-10-04 22:49:14 +0300

    changed port to 8081 due to conflicts

--- send_to_drive.py
+++ send_to_drive.py
@@ -22,7 +22,7 @@ def authenticate_google_calendar():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=8080)
+            creds = flow.run_local_server(port=8081)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return build('calendar', 'v3', credentials=creds)
